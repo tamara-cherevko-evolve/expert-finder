d90c55ca0d8f6ce4c1844d02ae92dfd6b474bbbb
tamara-cherevko-evolve <tamara.cherevko.mail@gmail.com>
2023-04-28 12:06:37 +0200

    add ui folder

---
+++ ui/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <h1 className="text-3xl font-bold bg-slate-400 text-center p-4">Expert Finder!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
